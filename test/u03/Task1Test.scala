f0f19982730fee91b29e40a7c975004324d9bdd3
fedpet <federico.pettinari2@studio.unibo.it>
2021-03-15 11:29:23 +0100

    Task1 flatMap test

--- test/u03/Task1Test.scala
+++ test/u03/Task1Test.scala
@@ -5,10 +5,17 @@ import org.junit.jupiter.api.Test
 import u03.Lists.List._
 
 class Task1Test {
-  @Test def testDrop(): Unit = {
-    val lst = Cons(10, Cons(20, Cons(30, Nil())))
+  private val lst = Cons(10, Cons(20, Cons(30, Nil())))
+
+  @Test def testDrop() {
     assertEquals(Cons(20, Cons(30, Nil())), drop(lst,1))
     assertEquals(Cons(30, Nil()), drop(lst,2))
     assertEquals(Nil(), drop(lst,5))
+  }
+
+  @Test def testFlatMap() {
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), flatMap(lst)(v => Cons(v+1, Nil())))
+    assertEquals(Cons(11, Cons(12, Cons(21, Cons(22, Cons(31, Cons(32, Nil())))))),
+      flatMap(lst)(v => Cons(v+1, Cons(v+2 , Nil()))))
   }
 }
