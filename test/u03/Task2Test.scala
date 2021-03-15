9758f34a8498e2541fd3c8097ce8ec2989e51b34
fedpet <federico.pettinari2@studio.unibo.it>
2021-03-15 12:49:25 +0100

    Task2 onlyCourses impl

--- test/u03/Task2Test.scala
+++ test/u03/Task2Test.scala
@@ -2,12 +2,19 @@ package u03
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import u03.Lists.List.Cons
-import u03.Lists.List.Nil
+import u02.Modules.Person
+import u02.Modules.Person.{Student, Teacher}
+import u03.Lists.List
+import u03.Lists.List.{Cons, Nil, flatMap}
 
 class Task2Test {
+  private def onlyCourses(l: List[Person]): List[String] = flatMap(l) {
+    case Teacher(_, c) => Cons(c, Nil())
+    case _ => Nil()
+  }
+
   @Test def testOnlyCourses() {
-    val l = Cons(Student("S1", 2018), Cons(Teacher("T1", "C1"), Cons(Student("S2", 2000), Cons(Teacher("T2", "C2"), Nil()))))
+    val l: List[Person] = Cons(Student("S1", 2018), Cons(Teacher("T1", "C1"), Cons(Student("S2", 2000), Cons(Teacher("T2", "C2"), Nil()))))
     assertEquals(Cons("C1", Cons("C2", Nil())), onlyCourses(l))
   }
 }
